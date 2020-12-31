9f331c87a454d51ec41d1978e2dfd1d279b3f1e3
Doctor5555 <5555thedoctor@gmail.com>
2020-12-31 16:44:06 +0000

    Logger now always outputs to a file, but not to stdout if not in debug mode

--- Visualiser/src/Log.cpp
+++ Visualiser/src/Log.cpp
@@ -8,10 +8,14 @@ std::shared_ptr<spdlog::logger> Log::s_Logger;
 void Log::Init()
 {
 	std::vector<spdlog::sink_ptr> logSinks;
+
+	#ifdef _DEBUG
 	logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+	logSinks[logSinks.size() - 1]->set_pattern("%^[%T] %n: %v%$");
+	#endif // _DEBUG
+
 	logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Visualiser.log", true));
-	logSinks[0]->set_pattern("%^[%T] %n: %v%$");
-	logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+	logSinks[logSinks.size() - 1]->set_pattern("[%T] [%l] %n: %v");
 
 	s_Logger = std::make_shared<spdlog::logger>("Visualiser", begin(logSinks), end(logSinks));
 	s_Logger->set_level(spdlog::level::trace);
